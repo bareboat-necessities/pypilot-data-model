6b87a5a4dc161043c5c8acb087edaeb9f2e9eba1
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-15 21:21:21 -0400

    Add navigation data model

---
+++ src/pypilot_data_model/navigation.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "core.hpp"
+
+namespace pypilot_data_model {
+
+template<typename Real = float>
+struct GpsData {
+    Setting<SensorSource> source;
+    RangeSetting<Real> rate_hz;
+
+    Stamped<Real> track_deg;
+    Stamped<Real> speed_kn;
+
+    Stamped<Real> fix_lat_deg;
+    Stamped<Real> fix_lon_deg;
+    Stamped<Real> fix_alt_m;
+    bool has_fix_json = false;
+
+    Stamped<Real> leeway_ground_deg;
+    Stamped<Real> compass_error_deg;
+    Stamped<Real> declination_deg;
+    Setting<int32_t> alignment_counter;
+
+    uint64_t last_update_us = 0;
+};
+
+template<typename Real = float>
+struct ApbData {
+    Setting<SensorSource> source;
+    Stamped<Real> track_deg;
+    Stamped<Real> xte_nmi;
+    RangeSetting<Real> xte_gain_deg_per_nmi;
+    uint64_t last_update_us = 0;
+};
+
+template<typename Real = float>
+struct NavigationData {
+    GpsData<Real> gps;
+    ApbData<Real> apb;
+};
+
+} // namespace pypilot_data_model
