baa8a5e67a8e15d9a452121d4243ae656cc5f6b4
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-15 21:22:40 -0400

    Add top-level data model

---
+++ src/pypilot_data_model/data_model.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "autopilot.hpp"
+#include "boatimu.hpp"
+#include "navigation.hpp"
+#include "wind.hpp"
+#include "water.hpp"
+#include "rudder.hpp"
+#include "servo.hpp"
+#include "pilots.hpp"
+#include "profile.hpp"
+
+namespace pypilot_data_model {
+
+template<typename Real = float>
+struct DataModel {
+    AutopilotData<Real> ap;
+    NavigationData<Real> navigation;
+    WindData<Real> wind;
+    WaterData<Real> water;
+    BoatImuData<Real> imu;
+    RudderData<Real> rudder;
+    ServoData<Real> servo;
+    PilotsData<Real> pilots;
+    ProfileData<Real> profile;
+};
+
+} // namespace pypilot_data_model
