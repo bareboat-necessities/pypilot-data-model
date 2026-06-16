68d5a2287db6ba0128aa213f14aebcbebed7e9c0
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-15 21:22:33 -0400

    Add profile placeholder model

---
+++ src/pypilot_data_model/profile.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace pypilot_data_model {
+
+template<typename Real = float>
+struct ProfileData {
+    bool has_profile = false;
+    bool has_profiles_json = false;
+};
+
+} // namespace pypilot_data_model
