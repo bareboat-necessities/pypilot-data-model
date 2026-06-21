cc7ae8bc1465919982d961c9f6c36b59b8eba78c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-21 15:44:23 -0400

    Add extended original pypilot value groups to DataModel

--- src/pypilot_data_model/data_model.hpp
+++ src/pypilot_data_model/data_model.hpp
@@ -9,20 +9,41 @@
 #include "servo.hpp"
 #include "pilots.hpp"
 #include "profile.hpp"
+#include "server.hpp"
+#include "status.hpp"
+#include "tack.hpp"
+#include "servo_telemetry.hpp"
+#include "imu_calibration.hpp"
+#include "pilot_io.hpp"
+#include "runtime_publication.hpp"
 
 namespace pypilot_data_model {
 
 template<typename Real = float>
 struct DataModel {
+    ServerData<Real> server;
+    StatusData<Real> status;
+
     AutopilotData<Real> ap;
     NavigationData<Real> navigation;
     WindData<Real> wind;
     WaterData<Real> water;
     BoatImuData<Real> imu;
+    ImuStateData<Real> imu_state;
+    ImuCalibrationData<Real> imu_calibration;
+
     RudderData<Real> rudder;
     ServoData<Real> servo;
+    ServoTelemetryData<Real> servo_telemetry;
+    ServoCalibrationData<Real> servo_calibration;
+
     PilotsData<Real> pilots;
+    PilotCommandData<Real> pilot_command;
+    PilotOutputData<Real> pilot_output;
+    TackData<Real> tack;
+
     ProfileData<Real> profile;
+    RuntimePublicationState<Real> runtime_publication;
 };
 
 } // namespace pypilot_data_model
