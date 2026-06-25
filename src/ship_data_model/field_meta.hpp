cba6468fbbe60afb4fcafab9ef30a7c30a18a18c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-25 19:31:03 -0400

    Make ship field meta header real

--- src/ship_data_model/field_meta.hpp
+++ src/ship_data_model/field_meta.hpp
@@ -1,2 +1,27 @@
 #pragma once
-#include "../../support/ship_data_model_bridge/field_meta.hpp"
+
+#include "field_id.hpp"
+
+namespace ship_data_model {
+
+enum class FieldType {
+    number,
+    boolean,
+    string_value,
+    mode_enum,
+    pilot_enum,
+    source_enum,
+    rudder_calibration_enum
+};
+
+struct FieldMeta {
+    FieldId id;
+    const char* pypilot_name;
+    const char* unit;
+    FieldType type;
+    bool writable;
+    bool persistent;
+    bool pilot_specific;
+};
+
+} // namespace ship_data_model
