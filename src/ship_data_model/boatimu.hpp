180fd6f29dfd69fd707489dac36b734c6968e68e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-25 22:55:07 -0400

    Merge IMU calibration into boat IMU header

--- src/ship_data_model/boatimu.hpp
+++ src/ship_data_model/boatimu.hpp
@@ -48,4 +48,22 @@ struct BoatImuData {
     RangeSetting<Real> headingraterate_lowpass_constant_0_1;
 };
 
+template<typename Real = float>
+struct ImuStateData {
+    Setting<bool> enabled;
+    Stamped<bool> present;
+    Stamped<bool> healthy;
+    char device[64] = {0};
+    char driver[64] = {0};
+    Stamped<Real> rate_hz;
+};
+
+template<typename Real = float>
+struct ImuCalibrationData {
+    Setting<bool> locked;
+    Stamped<Vec3<Real>> accel_bias_g;
+    Stamped<Vec3<Real>> gyro_bias_deg_s;
+    Stamped<Vec3<Real>> compass_offset;
+};
+
 } // namespace ship_data_model
